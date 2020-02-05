05bba15eb579226110f19c1118a83f449357d8e0
root <ubaidillahmh@gmail.com>
2020-02-05 08:59:40 +0700

    update upload

--- team_member_custom.php
+++ team_member_custom.php
@@ -18,6 +18,9 @@
         $member_website = esc_html( get_post_meta( $team_member->ID, 'member_website', true ) );
         $member_image = esc_html( get_post_meta( $team_member->ID, 'member_image', true ) );
 
+        get_template_part('content', 'single');
+        $image = get_post_meta($team_member->ID, 'member_image', true);
+
         ?>
         <table>
             <tr>
@@ -36,13 +39,23 @@
                 <td style="width: 100%">Website</td>
                 <td><input type="text" size="40" name="team_member_website" value="<?php echo $member_website; ?>" /></td>
             </tr>
-            <!-- <tr>
+            <tr>
+                <?php
+                    wp_nonce_field(plugin_basename(__FILE__), 'wp_custom_attachment_nonce');
+                    echo $image['url'];
+                ?>
                 <td style="width: 100%">Image</td>
-                <td><input type="file" size="40" name="team_member_image" /></td>
-            </tr> -->
+                <td><input type="file" size="40" id="wp_custom_attachment" name="team_member_image" /></td>
+            </tr>
         </table>
         <?php
     }
+
+    function update_edit_form() {
+        echo ' enctype="multipart/form-data"';
+    } 
+
+    add_action('post_edit_form_tag', 'update_edit_form');
 
     add_action( 'save_post', 'add_team_member_fields', 10, 2 );
 
@@ -62,12 +75,19 @@
             if ( isset( $_POST['team_member_website'] ) && $_POST['team_member_website'] != '' ) {
                 update_post_meta( $team_member_id, 'member_website', $_POST['team_member_website'] );
             }
-            // if (! empty($_FILES['team_member_image']['name'])) {
+            if (! empty($_FILES['team_member_image']['name'])) {
 
-            //     $upload = wp_upload_bits($_FILES['team_member_image']['name'],null, file_get_contents($_FILES['team_member_image']['tmp_name']));
+                // $upload = wp_upload_bits($_FILES['team_member_image']['name'],null, file_get_contents($_FILES['team_member_image']['tmp_name']));
+                if ( ! function_exists( 'wp_handle_upload' ) ) {
+                    require_once( ABSPATH . 'wp-admin/includes/file.php' );
+                }
 
-            //     update_post_meta( $team_member_id, 'member_image',$upload );
-            // }
+                $file   = $_FILES['team_member_image']; 
+                // $upload = wp_handle_upload($file, array('test_form' => false));
+                $upload = wp_upload_bits($_FILES['team_member_image']['name'], null, $_FILES['team_member_image']['tmp_name']);
+    
+                update_post_meta( $team_member_id, 'member_image', $upload );
+            }
         }
     }
 
@@ -75,17 +95,25 @@
     {
         get_header();
         $args = array('post_type' => 'team_members');
-        $the_query = new WP_Query($args);
-        // var_dump(json_encode($the_query));
+        $query = new WP_Query($args);
+        // var_dump(json_encode($query));
         $i = 1;
-        while ( $the_query->have_posts() ) : $the_query->the_post();
-        $id         = $the_query->post->ID;
+        while ( $query->have_posts() ) : $query->the_post();
+        $id         = $query->post->ID;
         $position   = get_post_meta($id, 'member_position', true);
         $email      = get_post_meta($id, 'member_email', true);
         $website    = get_post_meta($id, 'member_website', true); 
+        get_template_part('content', 'single');
+        $image = get_post_meta($id, 'member_image', true);
         // echo the_title();
         echo '<div class="grid-item">';
+        if($image != null)
+        {
+            echo    '<p><img href="'.$image['url'].'" alt="Photo Profile"/></p>';
+        }
+
         echo    '<p>'.the_title().'</p>';
+
         if ($pos != '0') {
             echo    '<p>'.$position.'</p>';
         }
